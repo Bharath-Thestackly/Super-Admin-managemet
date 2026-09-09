d225b27b93b2363bb24cd7d147b9e839b3e5074e
chinthasaidarao <saidaraochintha@thestackly.com>
2026-09-09 10:26:56 +0530

    OpenAPI

--- src/main/java/com/enterprise/superadmin/feature_management_service/config/OpenApiConfig.java
+++ src/main/java/com/enterprise/superadmin/feature_management_service/config/OpenApiConfig.java
@@ -1,9 +1,9 @@
 package com.enterprise.superadmin.feature_management_service.config;
 
-import org.springframework.boot.actuate.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class OpenApiConfig  {
 
