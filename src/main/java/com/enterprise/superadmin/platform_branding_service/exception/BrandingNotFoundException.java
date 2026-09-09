1b08d5ac0f632d6832b465fbc1d023af02be477f
yendutarun-thestackly <yendutarun@thestackly.com>
2026-09-09 21:52:09 +0530

    feat(branding): add exception for missing platform branding configuration

--- src/main/java/com/enterprise/superadmin/platform_branding_service/exception/BrandingNotFoundException.java
+++ src/main/java/com/enterprise/superadmin/platform_branding_service/exception/BrandingNotFoundException.java
@@ -1,4 +1,11 @@
 package com.enterprise.superadmin.platform_branding_service.exception;
 
-public class BrandingNotFoundException {
+/**
+ * Raised when the requested platform branding configuration cannot be found.
+ */
+public class BrandingNotFoundException extends RuntimeException {
+
+    public BrandingNotFoundException(String message) {
+        super(message);
+    }
 }
